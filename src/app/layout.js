2f737622d939f7f97c74adfe954a414c1c22bb31
Amirmohamads1383 <161164840+Amirmohamads1383@users.noreply.github.com>
2026-09-06 23:55:09 +0330

    Fix Top Header Component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
@@ -5,7 +7,11 @@ export default function RootLayout({ children }) {
     <html
       lang="fa" dir="rtl"
     >
-      <body className="">{children}</body>
+      <body className="bg-white font-IRANSansXV" cz-shortcut-listen="true">
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
